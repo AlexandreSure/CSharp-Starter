4537fabae32733ea03513c34cad74f92a18cb307
AlexandreSure <33261892+AlexandreSure@users.noreply.github.com>
2017-11-02 20:54:03 +0300

    Add files via upload

---
+++ homeWork1/task two/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region a
+            Console.WriteLine("Insert weight(kg):");
+            double weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insert height(m):");
+            double height = double.Parse(Console.ReadLine());
+            double BMI = weight / (height * height);
+            Console.WriteLine(BMI);
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
